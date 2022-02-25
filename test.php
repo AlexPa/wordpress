7cadf7b7b6dfd43b2e2aa321360b2672ab255ef1
AlexPa <a_l_e_x_1@inbox.ru>
2022-02-25 16:19:19 +0700

    commit

--- test.php
+++ test.php
@@ -1,6 +1,15 @@
 <?php
 error_reporting(E_ALL);
 
-$res = mail('a_l_e_x_1@inbox.ru', 'Theme', 'Message');
+$headers  = "From: balumba.site <mail@balumba.site>\n";
+$headers .= "Cc: balumba.site <mail@balumba.site>\n";
+$headers .= "X-Sender: balumba.site <mail@balumba.site>\n";
+$headers .= 'X-Mailer: PHP/' . phpversion();
+$headers .= "X-Priority: 1\n"; // Urgent message!
+$headers .= "Return-Path: mail@balumba.site\n"; // Return path for errors
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\n";
+
+$res = mail('a_l_e_x_1@inbox.ru', 'Theme', 'Message', $headers);
 
 var_dump($res);
